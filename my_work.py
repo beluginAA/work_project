bab27440d2fb07c2d3e0c00714c9c75544744744
Aleksandr Belugin <SasaBelugin@yandex.ru>
2023-01-24 14:59:56 +0300

    Добавьте файлы проекта.

---
+++ my_work.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+import docx
+import docx2txt
+import pypandoc
+import os
+import time
+import numpy as np
+from PIL import Image
+from docx.oxml import OxmlElement, ns
+from docx2python import docx2python
+from docx.shared import Pt, Mm
+from docx.enum.text import WD_ALIGN_PARAGRAPH, WD_PARAGRAPH_ALIGNMENT
+from docx.enum.style import WD_STYLE_TYPE
+from docx.enum.table import WD_ALIGN_VERTICAL
+from docx.enum.section import WD_ORIENT
+
+start_time = time.time()
+
+def new_document(pt = 14, line_spacing = 1.15, font_name = 'Times New Roman', left_margin = 30, right_margin = 15, top_margin = 20, bottom_margin = 20):
+
+    def adding_margins(name):
+        #def find_words(words):
+        #    for i in words:
+        #        if not any(map(str.isdigit, i)): 
+
+        text, foot_flag, num_footnotes, image_flag = [], False, [], False
+        document = docx2python(r'C:\Users\AlABelugin\Desktop\Project\Words\Documents\\' + str(name))
+        for obj in document.body:
+            if len(obj) > 1 and len(obj[0][0]) == 1:
+                numbers = [[obj[_][number][0] for number in range(len(obj[0]))] for _ in range(len(obj))]
+                text.append(numbers)
+                num_footnotes.append(0)
+            elif len(obj) == 1 and len(obj[0][0]) >= 1:
+                for line in range(len(obj[0][0])):
+                    if obj[0][0][line].find('.png') != -1:
+                        text.append(obj[0][0][line][obj[0][0][line].find('media') + 6 :][: obj[0][0][line][obj[0][0][line].find('media') + 6 :].find('----')])
+                        num_footnotes.append(0)
+                        image_flag = True
+                    elif obj[0][0][line].find('footnote') != -1:
+                        phrase = np.array(list(obj[0][0][line][: obj[0][0][line].find('----')]))
+                        while True:
+                            if phrase[0].isalpha() : break
+                            if phrase[0] == ' ': del phrase[0]
+                        for _ in range(12): phrase = np.insert(phrase, 0, ' ')
+                        phrase = ''.join(phrase)
+                        #find_words(phrase.split())
+                        text.append(phrase)
+                        num_footnotes.append(1)
+                        foot_flag = True
+                    else:
+                        #print(obj[0][0][line].split())
+                        phrase = list(obj[0][0][line])
+                        if obj[0][0][line].find('\t') != -1: 
+                            text.append(obj[0][0][line][obj[0][0][line].find('--\t') + 1 :])
+                            num_footnotes.append(0)
+                            continue
+                        if len(phrase) == 0: 
+                            text.append(obj[0][0][line])
+                            num_footnotes.append(0)
+                            continue
+                        if len(obj[0][0][line].split()) <= 10: 
+                            text.append(obj[0][0][line].capitalize())
+                            num_footnotes.append(0)
+                            continue
+                        while True:
+                            if phrase[0].isalpha() : break
+                            if phrase[0] == ' ': del phrase[0]
+                        for _ in range(12): phrase = np.insert(phrase, 0, ' ')
+                        phrase = ''.join(phrase)
+                        text.append(phrase)
+                        num_footnotes.append(0)
+        return text, num_footnotes, foot_flag, image_flag
+
+    def adding_list(name):
+        if text[0][name].find('-\t') != -1: paragrahp = new_doc.add_paragraph(text[0][name][text[0][name].find('\t') + 1 :], style = 'List Bullet')
+        else: paragrahp = new_doc.add_paragraph(text[0][name][text[name].find('\t') + 1 :], style = 'List Number')
+        p_fmt = paragrahp.paragraph_format
+        p_fmt.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        p_fmt.line_spacing = line_spacing
+        p_fmt.space_before = Pt(line_spacing)
+        p_fmt.space_after = Pt(line_spacing)
+        return paragrahp
+
+    def adding_heading(name):
+        if headings[name] and text[0][name - 1] != '' and name != 0:
+                paragrahp = new_doc.add_paragraph('')
+        paragrahp = new_doc.add_paragraph('')
+        run = paragrahp.add_run(text[0][name])
+        run.bold = True
+        run.font.size = Pt(pt+2)
+        p_fmt = paragrahp.paragraph_format
+        p_fmt.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        p_fmt.line_spacing = 1.5
+        p_fmt.space_before = Pt(1.5)
+        p_fmt.space_after = Pt(1.5)
+
+    def adding_paragraph(name):
+        if name != 0:
+            if pictures[name - 1] == 1 and text[0][name] != ' ':
+                paragrahp = new_doc.add_paragraph('')
+                run = paragrahp.add_run(text[0][name])
+                run.italic = True
+                run.font.size = Pt(pt - 2)
+                p_fmt = paragrahp.paragraph_format
+                p_fmt.alignment = WD_ALIGN_PARAGRAPH.CENTER
+            else:
+                paragrahp = new_doc.add_paragraph(text[0][name])
+                p_fmt = paragrahp.paragraph_format
+                p_fmt.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        p_fmt.line_spacing = line_spacing
+        p_fmt.space_before = Pt(line_spacing)
+        p_fmt.space_after = Pt(line_spacing)
+        return paragrahp
+
+    def adding_table(document, txt, name, pt):
+        table = document.add_table(rows = 0, cols = len(txt[name][0]), style='Table Grid')
+        for row in range(len(txt[name])):
+            rows = table.add_row().cells
+            for col in range(len(txt[name][0])):
+                cell_paragraphs = [paragraph for paragraph in table.cell(row ,col).paragraphs]
+                for paragraph in cell_paragraphs:
+                    p = paragraph._element
+                    p.getparent().remove(p)
+                    paragraph._p = paragraph._element = None
+                table.cell(row ,col).vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+                if row == 0: 
+                    p = table.cell(row, col).add_paragraph('')
+                    run = p.add_run(txt[name][row][col])
+                    run.bold = True
+                    run.font.size = Pt(pt+2)
+                    p.alignment=WD_ALIGN_PARAGRAPH.CENTER
+                else:
+                    p = table.cell(row, col).add_paragraph(txt[name][row][col])
+                    p.alignment=WD_ALIGN_PARAGRAPH.CENTER
+
+    def adding_picture(name):
+        with Image.open(r'C:\Users\AlABelugin\Desktop\Project\Words\Documents\\' + str(text[0][name])) as img:
+            width, height = img.size
+            img.save(r'C:\Users\AlABelugin\Desktop\Project\Words\Documents\\' + str(text[0][name]) , dpi=(800, 800))
+        if width > 700:
+            new_doc.add_picture(r'C:\Users\AlABelugin\Desktop\Project\Words\Documents\\' + str(text[0][name]), width = Mm(width/4.25), height = Mm(height/4.25))
+        else:
+            new_doc.add_picture(r'C:\Users\AlABelugin\Desktop\Project\Words\Documents\\' + str(text[0][name]), width = Mm(width/3.5), height = Mm(height/3.5))
+        new_doc.paragraphs[-1].alignment = WD_ALIGN_PARAGRAPH.CENTER
+        os.remove(r'C:\Users\AlABelugin\Desktop\Project\Words\Documents\\' + str(text[0][name]))
+
+    def adding_footnotes(name):
+        with open(r'C:\Users\AlABelugin\Desktop\Project\Words\Documents\\' + str(name[:name.find('.docx')]) + '.txt', 'r', encoding = 'utf8') as file:
+            f = file.readlines()
+            footnotes, summa = {}, 0
+            for word in range(len(f)):
+                if f[word].find('footnote') != -1:
+                    summa += 1
+                    footnotes[summa] = f[word][f[word].find('footnote') + 10 :][: f[word][f[word].find('footnote') + 10 :].find(']')]
+        os.remove(r'C:\Users\AlABelugin\Desktop\Project\Words\Documents\\' + str(name[:name.find('.docx')]) + '.txt')
+        return footnotes
+    
+    def add_page_number(paragraph):
+
+        def create_attribute(element, name, value):
+            element.set(ns.qn(name), value)
+
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        page_num_run = paragraph.add_run()
+        fldChar1 = OxmlElement('w:fldChar')
+        create_attribute(fldChar1, 'w:fldCharType', 'begin')
+        instrText = OxmlElement('w:instrText')
+        create_attribute(instrText, 'xml:space', 'preserve')
+        instrText.text = "PAGE"
+        fldChar2 = OxmlElement('w:fldChar')
+        create_attribute(fldChar2, 'w:fldCharType', 'end')
+        page_num_run._r.append(fldChar1)
+        page_num_run._r.append(instrText)
+        page_num_run._r.append(fldChar2)
+
+    text = adding_margins(str(filename))
+    if text[2]: 
+        docxFilename = r'C:\Users\AlABelugin\Desktop\Project\Words\Documents\\' + str(filename)
+        output = pypandoc.convert_file(docxFilename, to = 'asciidoc', outputfile = r'C:\Users\AlABelugin\Desktop\Project\Words\Documents\\' + str(filename[:filename.find('.docx')]) + '.txt')
+        # Возможные кодировки: asciidoc, asciidoctor, beamer, biblatex, bibtex, commonmark, commonmark_x, context, csljson, docbook, 
+        # docbook4, docbook5, docx, dokuwiki, dzslides, epub, epub2, epub3, fb2, gfm, haddock, html, html4, html5, icml, ipynb, 
+        # jats, jats_archiving, jats_articleauthoring, jats_publishing, jira, json, latex, man, markdown, markdown_github, 
+        # markdown_mmd, markdown_phpextra, markdown_strict, markua, mediawiki, ms, muse, native, odt, opendocument, opml, 
+        # org, pdf, plain, pptx, revealjs, rst, rtf, s5, slideous, slidy, tei, texinfo, textile, xwiki, zimwiki
+        footnotes = adding_footnotes(filename)
+    if text[3]: docx2txt.process(r'C:\Users\AlABelugin\Desktop\Project\Words\Documents\\' + str(filename),r"C:\Users\AlABelugin\Desktop\Project\Words\Documents")
+    new_doc = docx.Document()
+    new_doc.sections[0].orientation = WD_ORIENT.PORTRAIT
+    new_doc.sections[0].left_margin = Mm(left_margin)
+    new_doc.sections[0].right_margin = Mm(right_margin)
+    new_doc.sections[0].top_margin = Mm(top_margin)
+    new_doc.sections[0].bottom_margin = Mm(bottom_margin)
+    new_doc.styles['Normal'].font.name = font_name
+    new_doc.styles['Normal'].font.size = Pt(pt)
+    headings = [1 if type(text[0][j]) is not list and len(text[0][j].split()) <= 10 and text[0][j] != '' and text[0][j].find('\t') == -1  else 0 for j in range(len(text[0]))]
+    pictures, summa = [1 if type(text[0][phrase]) is not list and text[0][phrase].find('.png') != -1 else 0 for phrase in range(len(text[0]))], 0
+    for phrase in range(len(text[0])):
+        if type(text[0][phrase]) is list: adding_table(new_doc, text[0], phrase, pt)
+        elif text[0][phrase].find('.png') != -1: adding_picture(phrase)
+        elif text[0][phrase].find('\t') != -1: 
+            paragrahp = adding_list(phrase)
+            if text[1][phrase]:
+                summa += 1
+                run = paragrahp.add_run(' [' + str(footnotes[summa]) + ']')
+                run.bold = True
+                run.font.size = Pt(10)
+        elif len(text[0][phrase].split()) <= 10: adding_heading(phrase)
+        else: 
+            paragrahp = adding_paragraph(phrase)
+            if text[1][phrase]:
+                summa += 1
+                run = paragrahp.add_run(' [' + str(footnotes[summa]) + ']')
+                run.bold = True
+                run.font.size = Pt(10)
+    add_page_number(new_doc.sections[0].footer.paragraphs[0])
+    save_name = r'C:\Users\AlABelugin\Desktop\Project\Words\New Documents\\'+ '_' + str(filename)
+    new_doc.save(save_name)
+
+def delete_files():
+    directory = r'C:\Users\AlABelugin\Desktop\Project\Words\New Documents'
+    if os.path.isdir("New Documents"):
+        for filename in os.listdir(directory):
+            f = os.path.join(directory, filename)
+            if os.path.isfile(f) and filename.endswith('.docx'): os.remove(f)
+
+directory, files = r'C:\Users\AlABelugin\Desktop\Project\Words\Documents', []
+delete_files()
+if not os.path.isdir("New Documents"): os.mkdir("New Documents")
+assert len(os.listdir(directory)) > 0, 'Download files in the folder'
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f) and filename.endswith('.docx'): 
+        files.append(filename[: filename.find('.docx')])
+        new_document(14, 1.15, 'Times New Roman', 30, 15, 20, 20)
+
+end_time = time.time()
+print(end_time - start_time)
